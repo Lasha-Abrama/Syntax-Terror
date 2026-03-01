cba05d6133bd86d06cfdd84baba813c78c1e5409
Lasha Abramishvili <abramishvililasha25@gmail.com>
2026-03-01 23:32:51 +0400

    managed active class, added routes and middleware for current page...

--- index.js
+++ index.js
@@ -12,15 +12,36 @@ app.set("views", path.join(__dirname, "views"));
 // Serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
+
+// Middleware for current page
+app.use((req, res, next) => {
+  res.locals.currentPage = req.path;
+  next();
+});
+
 // Routes
 app.get("/", (req, res) => {
-  res.render("homepage", {
-    data,
-  });
+  res.render("homepage", { data });
+});
+
+app.get("/discover", (req, res) => {
+  res.render("discover");
+});
+
+app.get("/search", (req, res) => {
+  res.render("search");
+});
+
+app.get("/library", (req, res) => {
+  res.render("library");
+});
+
+app.get("/pins", (req, res) => {
+  res.render("pins");
 });
 
 app.get("/about", (req, res) => {
-  res.render("about", { page: "about" });
+  res.render("about");
 });
 
 app.listen(PORT, () => {
