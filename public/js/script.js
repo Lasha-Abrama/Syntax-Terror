2a0219b8bbcb253a4ed504b79c76937f00f39b16
Viktoria <vika.kazarova.vk@gmail.com>
2026-03-02 21:46:34 +0400

    Edited script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,14 @@
-document.querySelectorAll('.card-quantity').forEach(el => {
+document.querySelectorAll(".card-quantity").forEach((el) => {
   el.style.color = el.dataset.colour;
+});
+
+document.querySelectorAll(".podcasts").forEach((el) => {
+  const hex = el.dataset.colour; // "#85C585"
+  if (!hex) return;
+
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+
+  el.style.setProperty("--border-colour", `rgba(${r}, ${g}, ${b}, 0.6)`);
 });
