1dcd90cee1cf40aa8345d0018850d9705e6105dc
Lasha Abramishvili <abramishvililasha25@gmail.com>
2026-03-04 21:40:25 +0400

    add card-pin.ejs for library page

--- index.js
+++ index.js
@@ -21,27 +21,63 @@ app.use((req, res, next) => {
 
 // Routes
 app.get("/", (req, res) => {
-  res.render("homepage", { data });
+  res.render("homepage", { data, page: "homepage"});
 });
 
 app.get("/discover", (req, res) => {
-  res.render("discover");
+  res.render("discover", {
+    page: "discover"
+  });
 });
 
 app.get("/search", (req, res) => {
-  res.render("search", { data});
+  res.render("search", { data, page: "search"});
 });
 
 app.get("/library", (req, res) => {
-  res.render("library");
+  // 1. Liked playlist
+  const likedPlaylist = data.playlists.find(p => p.title === "Liked Songs");
+
+  // 2. Only Daft Punk and David Bowie
+  const selectedArtists = data.artists.filter(a =>
+    a.name === "Daft Punk" || a.name === "David Bowie"
+  );
+
+  // 3. Rest playlists
+  const otherPlaylists = data.playlists.filter(p => p.title !== "Liked Songs");
+
+  // 4. Selected mixes
+  const selectedMixes = data.mixes.filter(m =>
+    [
+      "Daily Mix 1",
+      "Daily Mix 2",
+      "Daily Mix 3",
+      "Rock Mix",
+      "Chill Mix",
+      "Pop Mix"
+    ].includes(m.title)
+  );
+
+  res.render("library", {
+    likedPlaylist,
+    selectedArtists,
+    otherPlaylists,
+    albums : data.albums,
+    selectedMixes,
+    page : "library"
+  });
 });
 
 app.get("/pins", (req, res) => {
-  res.render("pins");
+  res.render("pins", {
+    page: "pins"
+  });
 });
 
 app.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about", {
+    page: "about"
+  });
 });
 
 app.listen(PORT, () => {
